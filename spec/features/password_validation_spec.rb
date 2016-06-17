4c35299c07365787c82f59ab5633ae62683e849a
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-17 17:13:22 +0100

    trying to solve merge conflicts

--- spec/features/password_validation_spec.rb
+++
@@ -1,9 +0,0 @@
-feature 'Password validation' do
-  context 'Sign_up' do
-    scenario 'User enters wrong password in password confirmation box' do
-      expect { sign_up_passwords_do_not_match }.not_to change(User, :count)
-      expect(current_path).to eq('/users/new')
-      expect(page).to have_content 'Password and confirmation password do not match'
-    end
-  end 
-end
