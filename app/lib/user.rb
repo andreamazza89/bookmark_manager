5e293af1c6e79dc7302406a8788bb0b2b707af86
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-06-17 16:54:08 +0100

    More email validation

--- app/lib/user.rb
+++ app/lib/user.rb
@@ -10,11 +10,11 @@ class User
   attr_accessor :password_confirmation
 
   property :id, Serial
-  property :email, String, required: true
+  property :email, String, format: :email_address, required: true
   property :password_digest, String, length: 60
 
   validates_confirmation_of :password
-  #validates_presence_of     :email
+  #validates_presence_of    :email
   
   def password=(password)
     @password = password
